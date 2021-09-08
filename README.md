dfefb2c86801df7e788ac2fdfea0c2b787a5f8d9
Jorge Eduardo de Sousa Aguiar <sousa.aguiar@gmail.com>
2021-09-08 13:07:06 -0300

    Commit inicial

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# certificados_automaticos
-Gera certificados por meio de um template
+# CERTIFICADOS AUTOMÁTICOS
+Este programa gera certificados automáticos no formato ```.pdf```.
+
+A lista de inscritos deve estar no formato ```.xlsx```.
+
+A planilha com o nome dos inscritos, deverá ser a primeira da esquerda para a direita e o nome das colunas devem ser configurados em ```config.hjson```.
+
+O modelo do certificado deverá ser editado para inserir as informações de sua empresa.
+
+Todas as informações personalizadas como nome da empresa, nome do curso, data de início e fim devem ser editadas no arquivo ```config.hjson```.
+
+Para inserir o CPF no certificado, basta colocar a chave ```{{ cpf }}``` no local desejado.
+
+Antes de executar o programa, faça as configurações em ```config.hjson``` e instale os requerimentos digitando o comando ```pip install -r requeriments.txt``` no terminal.
+
+
+
---
+++ certificados_automaticos.py
@@ -0,0 +1,133 @@
+# IMPORTAÇÕES
+import pandas as pd
+import jinja2
+from docxtpl import DocxTemplate
+from docx2pdf import convert
+from datetime import date
+import logging
+import os
+import hjson
+
+
+###################################################################################################
+# CONFIGURAÇÃO DE LOGS
+
+logging.basicConfig(
+    level=logging.INFO,
+    # para formato mais simples, semelhante ao print(), pode-se usar:
+    # format="%(message)s",
+    format="%(asctime)s [%(name)s] [%(levelname)s]: %(message)s",
+    datefmt="%d/%m/%Y %H:%M:%S",
+)
+LOGGER = logging.getLogger("GERADOR DE CERTIFICADOS")
+
+###################################################################################################
+# IMPORTANDO CONFIGURAÇÕES DO ARQUIVO config.hjson
+config_data = {}
+if os.path.exists("config.hjson"):
+    with open("config.hjson") as config_file:
+        try:
+            config_data = hjson.load(config_file)
+        except hjson.HjsonDecodeError:
+            LOGGER.critical(
+                "Erro ao decodificar arquivo de configuração:", exc_info=True
+            )
+            exit(1)
+
+
+###################################################################################################
+# VARIÁVEIS PARA O CERTIFICADO
+meses = (
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro",
+)
+
+nome_curso = config_data.get("nome_do_curso", "INSERIR NOME DO CURSO EM config.hjson")
+nome_instituicao = config_data.get(
+    "nome_instituicao", "INSERIR NOME DA INSTITUIÇÃO EM config.hjson"
+)
+carga_horaria = config_data.get(
+    "carga_horaria", "INSERIR CARGA HORÁRIA EM config.hjson"
+)
+cidade = config_data.get("cidade")
+dia_inicio = int(config_data.get("dia_inicio", 1))
+mes_inicio = int(config_data.get("mes_inicio", 1))
+ano_inicio = int(config_data.get("ano_inicio", 9999))
+dia_fim = int(config_data.get("dia_fim", 1))
+mes_fim = int(config_data.get("mes_fim", 2))
+ano_fim = int(config_data.get("ano_fim", 9999))
+
+data_inicio = date(year=ano_inicio, month=mes_inicio, day=dia_inicio)
+data_fim = date(year=ano_fim, month=mes_fim, day=dia_fim)
+dias_de_curso = (data_fim - data_inicio).days
+mes_extenso = meses[(mes_fim) - 1]
+
+
+###################################################################################################
+# IMPORTAR LISTA DE INSCRITOS
+arquivo_inscricoes = config_data.get("arquivo_inscricoes")
+path = f"./{arquivo_inscricoes}"
+inscritos = pd.read_excel(path)
+inscritos.to_dict(orient="records")
+nome_coluna_inscritos = config_data.get("nome_coluna_inscritos")
+nome_coluna_cpf = config_data.get("nome_coluna_cpf")
+
+
+###################################################################################################
+# RENDERIZAÇÃO
+def gerar_certificado(inscrito: str, cpf):
+    # VARIÁVEIS PARA O CERTIFICADO
+    arquivo_template = f"./modelo_certificado.docx"
+    arquivo_destino = f"./certificados/certificado_{inscrito}.docx"
+    template = DocxTemplate(arquivo_template)
+    jinja_env = jinja2.Environment()
+
+    context = {
+        "nome_pessoa": inscrito,
+        "cpf": cpf,
+        "nome_instituicao": nome_instituicao,
+        "curso": nome_curso,
+        "carga_horaria": carga_horaria,
+        "cidade": cidade,
+        "data_inicio": f"{data_inicio.day}/{data_inicio.month}/{data_inicio.year}",
+        "data_fim": f"{data_fim.day}/{data_fim.month}/{data_fim.year}",
+        "dias_de_curso": dias_de_curso,
+        "dia": f"{data_fim.day}",
+        "mes_extenso": mes_extenso,
+        "ano": f"{data_fim.year}",
+    }
+
+    template.render(context, jinja_env)
+    template.save(arquivo_destino)
+    template = None
+    convert(
+        f"./certificados/certificado_{inscrito}.docx",
+        f"./certificados/certificado_{inscrito}.pdf",
+    )
+    os.remove(f"./certificados/certificado_{inscrito}.docx")
+    LOGGER.info(f"Gerado certificado de {inscrito}")
+
+
+contagem = 0
+
+# for inscrito, cpf in inscritos[f"{nome_coluna_inscritos}"][f"{nome_coluna_cpf}"]:
+#     gerar_certificado(inscrito, cpf)
+#     contagem += 1
+index = 0
+for inscrito in inscritos["Nome"]:
+    gerar_certificado(inscrito, inscritos["CPF"][index])
+    index += 1
+
+LOGGER.info(
+    f"{'Foram' if contagem >1 else 'Foi'} gerado{'s' if contagem > 0 else ''} {contagem} certificado{'s' if contagem > 0 else ''}."
+)
